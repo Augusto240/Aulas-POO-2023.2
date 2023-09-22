0571219c2fe80c7f6b1a0212b78c2a75d53b2f74
Augusto <129184930+Augusto240@users.noreply.github.com>
2023-09-22 16:35:10 -0300

    adicionando os exercicios 01 e 02

---
+++ exercicio01.py
@@ -0,0 +1,4 @@
+numero = int(input("Digite um número inteiro: "))
+
+if (numero > 0):
+    print("O número é positivo!")
---
+++ exercicio02.py
@@ -0,0 +1,7 @@
+nota1 = int(input("Digite a primeira nota: "))
+nota2 = int(input("Digite a segunda nota: "))
+
+media = (nota1 + nota2) / 2
+print(media)
+if (media >=30 and media <70):
+    print("Você está em RECUPERAÇÃO!!!")
